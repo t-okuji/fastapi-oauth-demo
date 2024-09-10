c0a49efb73a5b646fc8c4084710030f1126634ce
t-okuji <t.okuji34@gmail.com>
2024-09-10 10:13:29 +0900

    feat: Use state param for prevent CSRF attack

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+import secrets
+from typing import Annotated
+
 import requests
-from fastapi import FastAPI, Form, HTTPException, Response, status
+from fastapi import Cookie, FastAPI, Form, HTTPException, Response, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from settings import Settings
@@ -27,6 +30,10 @@ async def google_auth(response: Response):
     Returns:
         str: URL
     """
+
+    # Create state for prevent CSRF attack
+    state = secrets.token_urlsafe()
+
     google_auth_url = (
         f"{settings.GOOGLE_AUTH_URI}?"
         f"client_id={settings.GOOGLE_CLIENT_ID}&"
@@ -34,15 +41,28 @@ async def google_auth(response: Response):
         f"redirect_uri={settings.GOOGLE_REDIRECT_URI}&"
         f"response_mode=form_post&"
         f"response_type=code&"
-        f"scope=email"
+        f"scope=email&"
+        f"state={state}"
     )
     response = RedirectResponse(google_auth_url, status_code=302)
 
+    response.set_cookie(
+        key="authstate",
+        value=state,
+        httponly=True,
+        secure=True,
+        samesite="None",  # When developing (Lax for production)
+    )
     return response
 
 
 @app.post("/auth/google_auth/callback")
-async def google_auth_callback2(response: Response, code: str = Form()):
+async def google_auth_callback2(
+    response: Response,
+    code: str = Form(),
+    state: str = Form(),
+    authstate: Annotated[str | None, Cookie()] = None,
+):
     """Verify authorization code
 
     Args:
@@ -50,11 +70,20 @@ async def google_auth_callback2(response: Response, code: str = Form()):
         code (str, optional): Authorization code
 
     Raises:
-        HTTPException: Error
+        HTTPException: 401 500
 
     Returns:
         RedirectResponse: 302 response
     """
+
+    # Check state for prevent CSRF attack
+    if state != authstate:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
     body_data = {
         "grant_type": "authorization_code",
         "code": code,
