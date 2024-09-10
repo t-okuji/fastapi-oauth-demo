73c4d2a7e3c9a1d60134303ba834367a36c25f59
Tatsuya Okuji <t.okuji34@gmail.com>
2024-09-10 17:32:49 +0900

    feat: Create routing and apple oauth2 (#2)

---
+++ app/routers/google.py
@@ -0,0 +1,99 @@
+import secrets
+from typing import Annotated
+
+import requests
+from fastapi import APIRouter, Cookie, Form, HTTPException, Response, status
+from fastapi.responses import RedirectResponse
+from settings import Settings
+
+router = APIRouter()
+settings = Settings()
+
+
+@router.get("/auth/google_auth")
+async def google_auth(response: Response):
+    """Generate Google Auth URL
+
+    Returns:
+        str: URL
+    """
+
+    # Generate state for prevent CSRF attack
+    state = secrets.token_urlsafe()
+
+    google_auth_url = (
+        f"{settings.GOOGLE_AUTH_URI}?"
+        f"client_id={settings.GOOGLE_CLIENT_ID}&"
+        f"prompt=select_account consent&"
+        f"redirect_uri={settings.GOOGLE_REDIRECT_URI}&"
+        f"response_mode=form_post&"
+        f"response_type=code&"
+        f"scope=email&"
+        f"state={state}"
+    )
+    response = RedirectResponse(google_auth_url, status_code=302)
+
+    response.set_cookie(
+        key="authstate",
+        value=state,
+        httponly=True,
+        secure=True,
+        samesite="None",  # When developing (Lax for production)
+    )
+    return response
+
+
+@router.post("/auth/google_auth/callback")
+async def google_auth_callback(
+    response: Response,
+    code: str = Form(),
+    state: str = Form(),
+    authstate: Annotated[str | None, Cookie()] = None,
+):
+    """Verify authorization code
+
+    Args:
+        response (Response): fastapi Response
+        code (str, optional): Authorization code
+
+    Raises:
+        HTTPException: 401 500
+
+    Returns:
+        RedirectResponse: 302 response
+    """
+
+    # Check state for prevent CSRF attack
+    if state != authstate:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    body_data = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "client_id": settings.GOOGLE_CLIENT_ID,
+        "client_secret": settings.GOOGLE_CLIENT_SECRET,
+        "redirect_uri": settings.GOOGLE_REDIRECT_URI,
+    }
+
+    auth_response = requests.post(url=f"{settings.GOOGLE_TOKEN_URI}", data=body_data)
+    try:
+        auth_response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+    response = RedirectResponse(f"{settings.FRONT_URL}/me", status_code=302)
+
+    response.set_cookie(
+        key="token",
+        value="dummy_token",
+        httponly=True,
+        secure=True,
+        samesite="Lax",
+    )
+    return response
